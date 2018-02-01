60c4922f7e40920fa69bb00cff0c5832a556a279
Henry Eshbaugh <he915@ic.ac.uk>
2018-02-01 14:47:21 +0000

    First commit, WiFi/MQTT connect code

---
+++ client/mqtt-connect.py
@@ -0,0 +1,15 @@
+import network
+
+ap_if = network.WLAN(network.AP_IF)
+ap_if.active(false)
+
+sta_if = network.WLAN(network.STA_IF)
+sta_if.active(True)
+sta_if.connect("EEERover", "exhibition")
+
+import machine
+from umqtt.simple import MQTTClient
+client = MQTTClient(machine.unique_id(), "192.168.0.10")
+client.connect()
+
+client.publish("esys/avocadotoast/test", bytes("frukky frink", "utf-8"))
