77dc96a1465cd929b23752bd813b2110b386d20c
Henry Eshbaugh <he915@ic.ac.uk>
2018-02-11 18:19:12 +0000

    JSON database, eventually for flask

--- server/mqtt-subscribe.py
+++ server/mqtt-subscribe.py
@@ -1,11 +1,16 @@
 import paho.mqtt.client as mqtt
+from threading import Thread
+import json
+from tinydb import TinyDB, Query
+
+db = TinyDB("db.json")
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
     client.subscribe("esys/avocadotoast/#")
 
 def on_message(client, userdata, msg):
-    print(msg.topic + " " + str(msg.payload))
+    db.insert(json.loads(msg))
 
 client = mqtt.Client()
 client.on_connect = on_connect
@@ -13,5 +18,5 @@ client.on_message = on_message
 
 client.connect ("192.168.0.10", 1833, 60)
 
-client.loop_forever()
-
+mqttworker = Thread(target=client.loop_forever)
+mqttworker.start()
