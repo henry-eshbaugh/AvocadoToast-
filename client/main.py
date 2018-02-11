011ce73dca349ae667d5af06045e943eb117be49
AndreBharath <agb215@ic.ac.uk>
2018-02-11 18:02:52 +0000

    Tidied up code in lis3dh. Made changes to the JSON format to add a norm

--- client/main.py
+++ client/main.py
@@ -1,4 +1,5 @@
 from machine import Pin, I2C
+from math import sqrt
 import lis3dh
 import utime
 import ujson
@@ -16,13 +17,18 @@ def test():
     print("Byte 1 Masked:", val[1] & 0b00001111)
 
 while True:
-    xAccel = sensor.read_x()
-    yAccel = sensor.read_y()
-    zAccel = sensor.read_z()
-    temperature = sensor.read_temperature()
+#    rawx = sensor.read_raw_x()
+#    rawy = sensor.read_raw_y()
+#    rawz = sensor.read_raw_z()
+    x = sensor.read_x()
+    y = sensor.read_y()
+    z = sensor.read_z()
+    norm = sqrt(x**2 + y**2 + z**2)
+    temperature = sensor.read_raw_temperature()
     dateTime = utime.localtime()
     #print("Accelerometer values (m/s^2):\n    x: {0}    y: {1}    z: {2}".format(xAccel, yAccel, zAccel))
     #print("At time: {0}".format(dateTime))
-    data = ujson.dumps({"time": dateTime, "accelData": {'xAccel': xAccel, "yAccel": yAccel, "zAccel": zAccel}, "temperatureData": temperature})
+#    data = ujson.dumps({"time": dateTime, "accelData": {'x': x, "y": y, "z": z, 'norm': norm, 'rawx': rawx, 'rawy': rawy, 'rawz': rawz}, "temperatureData": temperature})
+    data = ujson.dumps({"time": dateTime, "accelData": {"x": x, "y": y, "z": z, "norm": norm}, "temperature": temperature})
     print(data)
-    utime.sleep(1)
+    utime.sleep(0.125)
