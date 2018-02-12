966f616c1bb88cf0c04263e83d705f3142e80182
Henry Eshbaugh <he915@ic.ac.uk>
2018-02-12 18:38:03 +0000

    Make sure db.json/mqtt works

--- WebStuff/startbootstrap-new-age-gh-pages/mqtt-subscribe.py
+++ WebStuff/startbootstrap-new-age-gh-pages/mqtt-subscribe.py
@@ -8,13 +8,12 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):
     with open('db.json', 'a') as f:
-        f.write(msg + '\n')
+        f.write(msg.payload.decode("utf-8") + '\n')
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect ("192.168.0.10", 1833, 60)
+client.connect ("192.168.0.10", 1883, 60)
 
-mqttworker = Thread(target=client.loop_forever)
-mqttworker.start()
+client.loop_forever()
--- server/mqtt-subscribe.py
+++ server/mqtt-subscribe.py
@@ -1,22 +1,21 @@
 import paho.mqtt.client as mqtt
 from threading import Thread
 import json
-from tinydb import TinyDB, Query
 
-db = TinyDB("db.json")
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
     client.subscribe("esys/avocadotoast/#")
 
 def on_message(client, userdata, msg):
-    db.insert(json.loads(msg))
+    #db.insert(json.loads(msg))
+    print(msg.payload.decode("utf-8"))
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect ("192.168.0.10", 1833, 60)
+client.connect ("192.168.0.10", 1883, 60)
 
 mqttworker = Thread(target=client.loop_forever)
 mqttworker.start()
