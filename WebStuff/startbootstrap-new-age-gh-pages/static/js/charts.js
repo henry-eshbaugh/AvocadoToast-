1c58fba0e18bed30da70761ebe21a02522d561f2
RyanArmiger <RyanArmiger@github.com>
2018-02-12 09:40:07 -0800

    test json in main

--- WebStuff/startbootstrap-new-age-gh-pages/static/js/charts.js
+++ WebStuff/startbootstrap-new-age-gh-pages/static/js/charts.js
@@ -1,6 +1,6 @@
 
 window.onload = function () {
-
+	UpdateJSON()
 	dashboardChart()
 	sleepBreakdown()
 	trendsChart()
--- WebStuff/startbootstrap-new-age-gh-pages/static/js/record.js
+++ WebStuff/startbootstrap-new-age-gh-pages/static/js/record.js
@@ -8,41 +8,45 @@
 //     });
 // });
 
-$.getJSON("/json.json", function(data) {
+//
+var record = {
+	//stored data
+	recordData	: [],
+	//function methods
+	classifData	: function() { //return recordData classified into Light, Deep and REM
+				var newList = [];
+				for (i = 0; i < this.recordData.length; i++){
+						if (this.recordData[i].y < 4){
+							newList.push({x: this.recordData[i].x, y: 3});
+						}
+						else if (this.recordData[i].y >= 3 && this.recordData[i].y < 5){
+							newList.push({x: this.recordData[i].x, y: 4});
+						}
+						else if (this.recordData[i].y >=5){
+							newList.push({x: this.recordData[i].x, y: 5});
+						}
+				}
+				return newList;
 
-      var JSONPoints = data;
-      console.log(JSONPoints);
+	},
+	totLight	: function() { //return total light sleep
+	},
+	totDeep		: function() { //return total deep sleep
+	},
+	totSleep	: function() { //return sum of light, deep, REM sleep
+	}
+
+}
+
+function UpdateJSON(){
+  $.getJSON("/json.json", function(data) {
+      for(i=0; i<data.length; i++){
+        console.log(data.length);
+        console.log(record.recordData);
+      record.recordData.push({x: data[i].x, y: data[i].y});
+    }
+    console.log("JSON updated");
+    console.log(record.recordData);
     });
     //Potential time issues
-
-
-
-
-// var record2 = {
-// 	//stored data
-// 	recordData()	: function () {JSON.parse(JSONPoints);},
-// 	//function methods
-// 	classifData	: function() { //return recordData classified into Light, Deep and REM
-// 				var newList = [];
-// 				for (i = 0; i < this.recordData.length; i++){
-// 						if (this.recordData[i].y < 4){
-// 							newList.push({x: this.recordData[i].x, y: 3});
-// 						}
-// 						else if (this.recordData[i].y >= 3 && this.recordData[i].y < 5){
-// 							newList.push({x: this.recordData[i].x, y: 4});
-// 						}
-// 						else if (this.recordData[i].y >=5){
-// 							newList.push({x: this.recordData[i].x, y: 5});
-// 						}
-// 				}
-// 				return newList;
-//
-// 	},
-// 	totLight	: function() { //return total light sleep
-// 	},
-// 	totDeep		: function() { //return total deep sleep
-// 	},
-// 	totSleep	: function() { //return sum of light, deep, REM sleep
-// 	}
-//
-// }
+}
