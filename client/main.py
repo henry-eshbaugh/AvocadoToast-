c80dbac8ead4866c51c99bda19654d1972460e26
AndreBharath <agb215@ic.ac.uk>
2018-02-12 16:09:21 +0000

    Moved mqtt connection to the main.py file

--- client/main.py
+++ client/main.py
@@ -1,12 +1,31 @@
 from machine import Pin, I2C
 from math import sqrt
+from umqtt.simple import MQTTClient
 import lis3dh
 import utime
 import ujson
+import network
 
 i2c = I2C(scl=Pin(5), sda=Pin(4), freq=10000)
 
 sensor = lis3dh.lis3dh(i2c)
+
+def mqtt_connect(client):
+    ap_if = network.WLAN(network.AP_IF)
+    ap_if.active(False)
+
+    sta_if = network.WLAN(network.STA_IF)
+    sta_if.active(True)
+    sta_if.connect("EEERover", "exhibition")
+    while not sta_if.isconnected():
+        utime.sleep(1)
+
+    #client = MQTTClient("avocadotoast", "192.168.0.10")
+    client.connect()
+
+#if __name__ == '__main__':
+client = MQTTClient("avocadotoast", "192.168.0.10")
+mqtt_connect(client)
 
 while True:
 #    rawx = sensor.read_raw_x()
@@ -21,6 +40,14 @@ while True:
     #print("Accelerometer values (m/s^2):\n    x: {0}    y: {1}    z: {2}".format(xAccel, yAccel, zAccel))
     #print("At time: {0}".format(dateTime))
 #    data = ujson.dumps({"time": dateTime, "accelData": {'x': x, "y": y, "z": z, 'norm': norm, 'rawx': rawx, 'rawy': rawy, 'rawz': rawz}, "temperatureData": temperature})
-    data = ujson.dumps({"time": dateTime, "accelData": {"x": x, "y": y, "z": z, "norm": norm}, "temperature": temperature})
-    print(data)
-    utime.sleep(0.125)
+    payload = ujson.dumps({"time": dateTime, "accelData": {"x": x, "y": y, "z": z, "norm": norm}, "temperature": temperature})
+    print(payload)
+    client.publish("esys/avocadotoast/sensor", bytes(payload, "utf-8"))
+    utime.sleep(1)
+
+
+
+
+
+
+
---
+++ client/mqtt_connect.py
@@ -0,0 +1,39 @@
+from umqtt.simple import MQTTClient
+from lis3dh import lis3dh
+import ujson
+import math
+import network
+import machine
+import utime
+
+if __name__ == '__main__':
+    ap_if = network.WLAN(network.AP_IF)
+    ap_if.active(false)
+    
+    sta_if = network.WLAN(network.STA_IF)
+    sta_if.active(True)
+    sta_if.connect("EEERover", "exhibition")
+    while not sta_if.isconnected():
+        utime.sleep(1)
+    
+    client = MQTTClient(machine.unique_id(), "192.168.0.10")
+    client.connect()
+    
+    i2c = I2C(sda=Pin(4), scl=Pin(5))
+    sensor = lis3dh(i2c)
+
+    while True:
+        x = sensor.readx()
+        y = sensor.ready()
+        z = sensor.readz()
+        t = sensor.readtemp()
+        m = math.sqrt(x**2 + y**2 + z**2)
+
+        # accel data as x, y, z vector triad and scalar sc
+        # t for temperature
+        payload = ujson.dumps({'x' : x, 'y' : y, 'z' : z, 't' : t, 'sc' : m})
+        client.publish("esys/avocadotoast/sensor", bytes(payload, "utf-8"))
+        utime.sleep(1)
+
+
+# client.publish("esys/avocadotoast/test", bytes("frukky frink", "utf-8"))
