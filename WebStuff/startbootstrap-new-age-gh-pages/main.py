403361cbca407f7360beb69fa3eac5c7235c7c1f
TrebleStick <jcp115@ic.ac.uk>
2018-02-13 11:43:04 +0000

    commit working site and host without css before editing in a different branch

--- WebStuff/startbootstrap-new-age-gh-pages/main.py
+++ WebStuff/startbootstrap-new-age-gh-pages/main.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template
 from flask import jsonify
 from flask import Response
 app = Flask(__name__)
-
-
+# app.static_folder = 'static'
+# return render_template(content)
 @app.route("/")
 @app.route('/index')
 def index():
