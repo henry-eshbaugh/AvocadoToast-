5a6a94805ccd4a50a0ee6a16d42da667b4a16306
AndreBharath <agb215@ic.ac.uk>
2018-02-14 23:28:59 +0000

    Calculate sleep activity

--- client/main.py
+++ client/main.py
@@ -10,6 +10,7 @@ i2c = I2C(scl=Pin(5), sda=Pin(4), freq=10000)
 
 sensor = lis3dh.lis3dh(i2c)
 
+# establishes a connection with the MQTT broker
 def mqtt_connect(client):
     ap_if = network.WLAN(network.AP_IF)
     ap_if.active(False)
@@ -17,57 +18,111 @@ def mqtt_connect(client):
     sta_if = network.WLAN(network.STA_IF)
     sta_if.active(True)
     sta_if.connect("EEERover", "exhibition")
+
+# wait for the connection to be made
     while not sta_if.isconnected():
         utime.sleep(1)
 
     client.connect()
 
-def take_readings(n):
+# takes n readings of data from the accelerometer registers
+def take_readings(n=20):
     cumX = 0
     cumY = 0
-    cumZ = 0
     cumTemp = 0
 
-    maxX = -1000
-    maxY = -1000
-    maxZ = -1000
-
+# average the accelerometer readings to reduce the effect of noise
     for i in range(n):
-        xreading = sensor.read_x()
-        yreading = sensor.read_y()
-        zreading = sensor.read_z()
-        tempreading = sensor.read_raw_temperature()
-
-        cumX += xreading
-        cumY += yreading
-        cumZ += zreading
-
+        cumX += sensor.read_x()
+        cumY += sensor.read_y()
         cumTemp += sensor.read_raw_temperature()
-        maxX = max(maxX, xreading)
-        maxY = max(maxY, yreading)
-        maxZ = max(maxZ, zreading)
 
         utime.sleep_us(1000)
 
     x = cumX/n
     y = cumY/n
-    z = cumZ/n    
-    norm = sqrt(x**2 + y**2 + z**2)
+
+# take the L2 norm of the accelerometer values
+# we found that including the acceleration in the z axis reduced
+# the detectability of sudden movements in bed
+    xynorm = sqrt(x**2 + y**2)
     temperature = cumTemp/n
     dateTime = utime.localtime()
 
-    return {"time": dateTime, "accelData": {"x": x, "y": y, "z": z, "norm": norm, "max_x": maxX, "max_y": maxY, "max_z": maxZ}, "temperature": temperature}
+    return {"time": dateTime, "xynorm": xynorm, "temperature": temperature}
+   
+
+# count the number of readings above a threshold value within an array
+def activity_in_chunk(chunk, thresh = 0.08):
+    count = 0
+    for reading in chunk:
+        if abs(reading) > thresh:
+            count += 1
     
+    return count
+
+
+# take the difference between consecutive values in an array
+def arr_diff(arr):
+    return [j-i for i, j in zip(arr[:-1], arr[1:])]
+
+
+def add_to_window(activityCount, activityWindow):
+    activityWindow = [activityCount] + activityWindow
+    if len(activityWindow) > 5:
+        return activityWindow[:-1]
+    else:
+        return activityWindow
+
+def process_window(activityWindow, weights = [0.04, 0.2, 1, 0.2, 0.04]):
+    if len(activityWindow) < 5:
+        return sum(i*j for i,j in zip(activityWindow, weights[2:]))
+    else:
+        return sum(i*j for i,j in zip(activityWindow, weights))
+
+
+def construct_payload(state, time, activity, temp):
+    adjustedTime = time[0], time[1], time[2], time[3], time[4]+1, time[5], time[6], time[7]
+    return ujson.dumps({"state": state, "time": adjustedTime, "activity": activity, "temperature": temp})
 
 #client = MQTTClient("avocadotoast", "192.168.0.10")
 #mqtt_connect(client)
 
+#client.publish("esys/avocadotoast/begin", bytes("NEW_SESSION", "utf-8"))
+
+
+
+curTime = utime.localtime()
+newSession = {"state": "NEW_SESSION", "time": curTime}
+newSessionJSON = ujson.dumps(newSession)
+print(newSessionJSON)
+#client.publish("esys/avocadotoast/startup", bytes(newSessionJson), "utf-8")
+
+chunkData = []
+activityWindow = []
+payloadQueue = []
+
 while True:
-    data = take_readings(20)    
-    payload = ujson.dumps(data)
-    print(payload)
-#   client.publish("esys/avocadotoast/sensor", bytes(payload, "utf-8"))
+
+    curData = take_readings(20)
+    
+    if curData["time"][:5] == curTime[:5]:
+        chunkData.append(curData["xynorm"])
+    else:
+        xyDiffNorm = arr_diff(chunkData)      
+        activity_count = activity_in_chunk(xyDiffNorm)
+        activityWindow = add_to_window(activity_count, activityWindow)
+        
+        activity = process_window(activityWindow) 
+        
+        payload = construct_payload("MEASURING", curTime, activity, curData["temperature"])    
+            
+#        if len(payloadQueue) == 3:
+#            print(payloadQueue.pop())
+        
+        print(payload)
+#        client.publish("esys/avocadotoast/activity", bytes(payload, "utf-8"))
+        curTime = utime.localtime()
+        chunkData = [] 
+         
     utime.sleep_ms(500)
-
-
-
--- server/plot_data2.py
+++ server/plot_data2.py
@@ -10,35 +10,14 @@ def accel_norm(data):
     
     return x, y, z, np.linalg.norm(np.asarray([x, y, z])), np.linalg.norm(np.asarray([x, y]))
 
-def chunk(xs, n):
-    def chunk2(arr, nReadings):
-        nReadings = max(1, nReadings)
-        return (arr[i:i+nReadings] for i in range(0, len(arr), nReadings))
-    return filter(lambda x: len(x) == n, chunk2(xs, n))
 
-def active_in_min(chunk, thresh = 0.08):
-    ret = 0
-    for reading in chunk:
-        if abs(reading) > thresh:
-            ret += 1
-    return ret
+def activity_in_chunk(chunk, thresh = 0.08):
+    return len(np.where(abs(chunk) > thresh)[0])
 
-#def drop_until_sleep(chunks, thresh=2):
-#    idx, count = 0, 0
-#    for minute in chunks:
-#        if active_in_min(minute) < thresh:
-#            count += 1
-#        else:
-#            count = 0
-#        if count == 5:
-#            return chunks[idx+1:]
-#        idx += 1
+def activity_window(activities, weights = [0.04, 0.2, 1, 0.2, 0.04]):
+    return np.convolve(activities, weights, 'same')    
 
-def activity(minutes):
-     return list(map(lambda x: 1.06*x[0] + 0.54 * x[1] + 0.58 * x[2] + 0.76 * x[3] + 2.3 * x[4] + 0.74 * x[5] + 0.67 * x[6], chunk(list(map(active_in_min, minutes)), 7)))
-    
-
-with open("sleepyandre2.txt") as f:
+with open("sleepyandre3.txt") as f:
     readings = f.readlines()
 
 readings = [json.loads(reading.strip()) for reading in readings]
@@ -47,21 +26,26 @@ dataArr = np.asarray(readings)
 
 accel_norm_vec = np.vectorize(accel_norm)
 xArr, yArr, zArr, xyzNormArr, xyNormArr = accel_norm_vec(dataArr)
-xdiff = [t - s for s, t in zip(xArr, xArr[1:])]
-ydiff = [t - s + 2 for s, t in zip(yArr, yArr[1:])]
-zdiff = [t - s + 4 for s, t in zip(zArr, zArr[1:])]
-xydiffnorm = [math.sqrt(s**2 + t**2) + 6 for s, t in zip(xdiff, ydiff)]
-active = activity(chunk(xydiffnorm, 60))
+xDiff = np.ediff1d(xArr)
+yDiff = np.ediff1d(yArr)
+zDiff = np.ediff1d(zArr) 
+xyDiffNorm = np.ediff1d(xyNormArr)
+
+xyDiffNormChunks = np.array_split(xyDiffNorm, int(len(xyDiffNorm)/60))
+activityChunks = np.vectorize(activity_in_chunk)(xyDiffNormChunks) 
+weightedActivities = activity_window(activityChunks)
+
+#active = activity(chunk(xydiffnorm, 60))
 #p1 = plt.plot(xArr, label='x')
 #p2 = plt.plot(yArr, label='y')
 #p3 = plt.plot(zArr-2, label='z')
 #p4 = plt.plot(xyNormArr+3, label='norm(xy)')
 #p5 = plt.plot(xyzNormArr, label='norm(xyz)')
-#plt.plot(xdiff, label='xdiff')
-#plt.plot(ydiff, label='ydiff')
-#plt.plot(zdiff, label='zdiff')
-#plt.plot(xydiffnorm, label='xydiffnorm')
-plt.plot(active, label='active')
+plt.plot(xDiff, label='xdiff')
+plt.plot(yDiff + 2, label='ydiff')
+plt.plot(zDiff + 4, label='zdiff')
+plt.plot(xyDiffNorm + 6, label='xydiffnorm')
+#plt.plot(active, label='active')
 
 plt.legend()
 plt.show()
