b99703d515d5ce255d47ac88cbb614956328c2e1
RyanArmiger <RyanArmiger@github.com>
2018-02-15 12:32:06 +0000

    server time added

--- WebStuff/startbootstrap-new-age-gh-pages/mqtt-subscribe.py
+++ WebStuff/startbootstrap-new-age-gh-pages/mqtt-subscribe.py
@@ -1,6 +1,7 @@
 import paho.mqtt.client as mqtt
 from threading import Thread
 import json
+from datetime import datetime
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
@@ -9,9 +10,10 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     with open('db1.json', 'a') as f:
         raw_payload = msg.payload.decode("utf-8")
-        dat = json.dumps(raw_payload)
-        dat['recvtime'] = time.localtime()
-        f.write(json.loads(dat) + '\n')
+        dat = json.loads(raw_payload)
+        t = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        dat['recvtime'] = t
+        f.write(json.dumps(dat) + '\n')
 
 client = mqtt.Client()
 client.on_connect = on_connect
